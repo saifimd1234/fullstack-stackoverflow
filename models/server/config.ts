f642df619dac291426903127b29e799437f19008
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-12-24 23:44:48 +0530

    Update config to accept tablesDb.
    Appwrite is moving from NoSQL-like terms (Collections) to relational terms (Tables) to better reflect its underlying architecture.

--- models/server/config.ts
+++ models/server/config.ts
@@ -1,5 +1,5 @@
 import env from "@/env";
-import { Client, Avatars, Databases, Functions, Storage, Users } from "node-appwrite";
+import { Client, Avatars, Databases, Functions, Storage, Users, TablesDB } from "node-appwrite";
 
 let client = new Client();
 
@@ -9,10 +9,11 @@ client
     .setKey(env.appwrite.apiKey) // Your secret API key
     ;
 
-const database = new Databases(client);
+const databases = new Databases(client);
+const tablesDb = new TablesDB(client);
 const storage = new Storage(client);
 const functions = new Functions(client);
 const avatars = new Avatars(client);
 const users = new Users(client);
 
-export { client, database, storage, functions, avatars, users };
+export { client, databases, tablesDb, storage, functions, avatars, users };
